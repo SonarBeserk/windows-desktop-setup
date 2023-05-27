0c1b838ddb8048b90e81d27ff04d5c24d2925c48
SonarBeserk <sonarbeserk@gmail.com>
2023-05-27 00:21:43 -0400

    Add script to help clean up windows ui issues

---
+++ scripts/clean-ui.ps1
@@ -0,0 +1,53 @@
+# Based on parts from https://github.com/Sycnex/Windows10Debloater/blob/master/Individual%20Scripts/Protect%20Privacy
+
+#Stops Cortana from being used as part of your Windows Search Function
+Write-Output "Stopping Cortana from being used as part of your Windows Search Function"
+$Search = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"
+If (Test-Path $Search) {
+    Set-ItemProperty $Search AllowCortana -Value 0 
+}
+
+#Disables Web Search in Start Menu
+Write-Output "Disabling Bing Search in Start Menu"
+$WebSearch = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"
+Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" BingSearchEnabled -Value 0 
+If (!(Test-Path $WebSearch)) {
+    New-Item $WebSearch
+}
+Set-ItemProperty $WebSearch DisableWebSearch -Value 1 
+
+#Prevents bloatware applications from returning and removes Start Menu suggestions               
+Write-Output "Adding Registry key to prevent bloatware apps from returning"
+$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent"
+$registryOEM = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager"
+If (!(Test-Path $registryPath)) { 
+    New-Item $registryPath
+}
+Set-ItemProperty $registryPath DisableWindowsConsumerFeatures -Value 1 
+
+If (!(Test-Path $registryOEM)) {
+    New-Item $registryOEM
+}
+
+Set-ItemProperty $registryOEM  ContentDeliveryAllowed -Value 0
+Set-ItemProperty $registryOEM  OemPreInstalledAppsEnabled -Value 0
+Set-ItemProperty $registryOEM  PreInstalledAppsEnabled -Value 0
+Set-ItemProperty $registryOEM  PreInstalledAppsEverEnabled -Value 0
+Set-ItemProperty $registryOEM  SilentInstalledAppsEnabled -Value 0
+Set-ItemProperty $registryOEM  SystemPaneSuggestionsEnabled -Value 0
+
+#Disables live tiles
+Write-Output "Disabling live tiles"
+$Live = "HKCU:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\PushNotifications"    
+If (!(Test-Path $Live)) {      
+    New-Item $Live
+}
+Set-ItemProperty $Live  NoTileApplicationNotification -Value 1 
+
+#Disables People icon on Taskbar
+Write-Output "Disabling People icon on Taskbar"
+$People = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People"    
+If (!(Test-Path $People)) {
+    New-Item $People
+}
+Set-ItemProperty $People  PeopleBand -Value 0
